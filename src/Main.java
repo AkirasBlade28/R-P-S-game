2c8a4baa7dd322d5627c3de386bf32ee532ff0d6
Miloš Janković <95304147+AkirasBlade28@users.noreply.github.com>
2021-12-23 11:52:59 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,130 +14,120 @@ p	// 1) Gen.  ran. num. from 1 to 3
 	//p>r && p<s   \\rules// ||> wins to||< loses to|| (6)
 	//s>p && s<r   
 
-	
-
-import java.util.Scanner;
+	import java.util.Scanner;
 
 public class Main {
 
 	@SuppressWarnings("resource")
-
 	public static void main(String[] args) {
-		
-	
-	Scanner in = new Scanner (System.in);
-	
-	
-	// actual flow//
-	String userIn="User input: ";
-	String compIn="Computer input: ";
-	String userWins="User wins!";
-	String compWins="Computer wins!";
-	int compScore=0;
-	int userScore=0;
-	
-	
-	
-	System.out.println("Wellcome to 'Rock, Paper, Scissors' Game!\n\n");
-	System.out.println("To play the game with the computer\n"
-			+ "input R, P, or S for the inicials\n"
-			+ "of Rock, Paper or Scissors.\n\n");
+		Scanner in = new Scanner (System.in);
+
+		String userIn="User input: ";
+		String compIn="Computer input: ";
+		String userWins="User wins!";
+		String compWins="Computer wins!";
+		int compScore=0;
+		int userScore=0;
+
+		System.out.println("Wellcome to 'Rock, Paper, Scissors' Game!\n\n");
+		System.out.println("To play the game with the computer\n"
+				+ "input R, P, or S for the inicials\n"
+				+ "of Rock, Paper or Scissors.\n\n");
 	
 	
 	
-	for(int totTries = 1;  ; ++totTries) //loop without condition to terminate+check num of inputs
-	{                                                    //config if u want the game to have  limit of tries
-			char userInput=in.next().charAt(0); // prompt for input
-		
-		
-		
-			switch(userInput) { 
-			
-			case 'R','r':
-				 System.out.println(userIn+"rock.");
-				 
-				 switch(compInput()) {
-				 
-				 case 'R':
-					 System.out.println(compIn+"rock.");
-					 System.out.println("Eaven!\n"); 
-				 break; 
-				 
-				 case 'P':
-					 System.out.println(compIn+"paper.");
-					 System.out.println(compWins+"\n");
-					 ++compScore;
-				 break;
-					 
-				 case 'S':
-					 System.out.println(compIn+"scissors");
-					 System.out.println(userWins+"\n");
-					 ++userScore;
+		for(int totTries = 1;  ; ++totTries) //loop without condition to terminate+check num of inputs
+		{                                                    //config if u want the game to have  limit of tries
+				char userInput=in.next().charAt(0); // prompt for input
+
+
+
+				switch(userInput) { 
+
+				case 'R','r':
+					 System.out.println(userIn+"rock.");
+
+					 switch(compInput()) {
+
+					 case 'R':
+						 System.out.println(compIn+"rock.");
+						 System.out.println("Eaven!\n"); 
+					 break; 
+
+					 case 'P':
+						 System.out.println(compIn+"paper.");
+						 System.out.println(compWins+"\n");
+						 ++compScore;
 					 break;
-				 }
-			break;
-				
-			case 'P','p':
-				System.out.println(userIn+"peper.");
-			
-				switch(compInput()) {
-				 
-				case 'R':
-					 System.out.println(compIn+"rock.");
-					 System.out.println(userWins+"\n");
-					 ++userScore;
-				 break; 
-				 
-				 case 'P':
-					 System.out.println(compIn+"paper.");
-					 System.out.println("Eaven!\n");
-				 break;
-					 
-				 case 'S':
-					 System.out.println(compIn+"scissors");
-					 System.out.println(compWins+"\n");
-					 ++compScore;
-					 break;
-				 }
-			break;
-			case 'S','s':
-				System.out.println(userIn+"scissors.");
-			
-			    switch(compInput()) {
-			 
-			    case 'R':
-			    	System.out.println(compIn+"rock.");
-			    	System.out.println(compWins+"\n");
-			    	++compScore;
-			    break; 
-			 
-			    case 'P':
-			    	System.out.println(compIn+"peper");
-			    	System.out.println(userWins+"\n");
-			    	++userScore;
-			    	
-			    break;
-				 
-			    case 'S':
-			    	System.out.println(compIn+"scissors.");
-			    	System.out.println("Eaven!\n");
+
+					 case 'S':
+						 System.out.println(compIn+"scissors");
+						 System.out.println(userWins+"\n");
+						 ++userScore;
+						 break;
+					 }
 				break;
-			 }
-			break;
-			default:
-				System.out.println("Error: invalid input!");
-			}
-		
-			System.out.println("User scores: "+userScore+".\n");
-			System.out.println("Computer scores: "+compScore+".\n");
-			System.out.println("Total inputs: "+totTries+".\n");//test, print total tries	
-		
-	}
+
+				case 'P','p':
+					System.out.println(userIn+"peper.");
+
+					switch(compInput()) {
+
+					case 'R':
+						 System.out.println(compIn+"rock.");
+						 System.out.println(userWins+"\n");
+						 ++userScore;
+					 break; 
+
+					 case 'P':
+						 System.out.println(compIn+"paper.");
+						 System.out.println("Eaven!\n");
+					 break;
+
+					 case 'S':
+						 System.out.println(compIn+"scissors");
+						 System.out.println(compWins+"\n");
+						 ++compScore;
+						 break;
+					 }
+				break;
+				case 'S','s':
+					System.out.println(userIn+"scissors.");
+
+				    switch(compInput()) {
+
+				    case 'R':
+					System.out.println(compIn+"rock.");
+					System.out.println(compWins+"\n");
+					++compScore;
+				    break; 
+
+				    case 'P':
+					System.out.println(compIn+"peper");
+					System.out.println(userWins+"\n");
+					++userScore;
+
+				    break;
+
+				    case 'S':
+					System.out.println(compIn+"scissors.");
+					System.out.println("Eaven!\n");
+					break;
+				 }
+				break;
+				default:
+					System.out.println("Error: invalid input!");
+				}
+
+				System.out.println("User scores: "+userScore+".\n");
+				System.out.println("Computer scores: "+compScore+".\n");
+				System.out.println("Total inputs: "+totTries+".\n");//test, print total tries	
+
+		}
 	
 	}
 	static int compInput() // (1, 2)
 	{
-		
 		double ranNumber=Math.random()*3;//if (int)Math.Random() is used to convert from double to int straight ahead
 		int ranNumber00=(int)ranNumber; // the random number wont be 'truly' random. It'll be stuck to one number only
 		switch (ranNumber00) {
@@ -147,12 +137,9 @@ public class Main {
 			return 'P';
 		case 2:
 			return 'S';
-		
 		default:		//this will return 'white space' if Math.random() hits 'run time error'
-			return ' ';
-			
+			return ' ';	
 		}
-	
 	}
 }
 
